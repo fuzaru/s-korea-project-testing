70688af01a6ad23d6bb21c2d4b561e6c097b1424
AvaRiz <jaynielacusapernecita@gmail.com>
2026-02-20 13:18:35 +0800

    Added @moduledoc for create_account_live_test.exs

--- test/medigrand_web/live/create_account_live_test.exs
+++ test/medigrand_web/live/create_account_live_test.exs
@@ -1,4 +1,7 @@
 defmodule MedigrandWeb.CreateAccountLiveTest do
+  @moduledoc """
+  This module tests the CreateAccountLive live view, which is responsible for rendering the create account page and handling user interactions related to account creation.
+  """
   use MedigrandWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
