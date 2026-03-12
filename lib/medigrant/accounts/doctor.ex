d238c0a19d7ddb06ee43775ce2aa568250aadfa1
Ramsey Lagus <22-00862@g.batstate-u.edu.ph>
2026-03-12 11:50:36 +0800

    created  basic doctor schema

---
+++ lib/medigrant/accounts/doctor.ex
@@ -0,0 +1,24 @@
+defmodulde Medigrant.Accounts.Doctor do
+  @moduledoc """
+  Doctor schema.
+  It includes fields for name, specialization, and license number.
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "doctors" do
+    field :name, :string
+    field :specialization, :string
+    field :license_number, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(doctor, attrs) do
+    doctor
+    |> cast(attrs, [:name, :specialization, :license_number])
+    |> validate_required([:name, :specialization, :license_number])
+    |> unique_constraint(:license_number)
+  end
+end
