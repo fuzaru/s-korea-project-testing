07337400fe8802f21ab24ec336bc7c819bf05604
Ramsey Lagus <22-00862@g.batstate-u.edu.ph>
2026-03-12 13:05:41 +0800

    add migration files for patients and doctors schemas

---
+++ priv/repo/migrations/20260312045751_patients.exs
@@ -0,0 +1,6 @@
+defmodule Medigrant.Repo.Migrations.Patients do
+  use Ecto.Migration
+
+  def change do
+  end
+end
---
+++ priv/repo/migrations/20260312045817_doctors.exs
@@ -0,0 +1,7 @@
+defmodule Medigrant.Repo.Migrations.Doctors do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
