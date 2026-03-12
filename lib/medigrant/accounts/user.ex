9deac7111bc3cef4690d417c2f2fb82b00d6c4eb
Ramsey Lagus <22-00862@g.batstate-u.edu.ph>
2026-03-12 11:27:22 +0800

    basic user schema created inside accounts folder

--- lib/medigrant/accounts/user.ex
+++ lib/medigrant/accounts/user.ex
@@ -1,13 +1,10 @@
 defmodule Medigrant.Accounts.User do
-<<<<<<< HEAD
   @moduledoc """
   The User schema represents a user in the system.
   It includes fields for email, name, and password hash.
   The changeset function is used to validate and cast user data when creating or updating a user record.
   It ensures that the email, name, and password hash are present and that the email is unique in the database.
   """
-=======
->>>>>>> c183e95 (Remove Medigrant.Accounts.User module)
   use Ecto.Schema
   import Ecto.Changeset
 
