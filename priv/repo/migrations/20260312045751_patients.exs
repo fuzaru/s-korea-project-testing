e6693586c3d25e6a78686578da2d8c57206891e3
Ramsey Lagus <22-00862@g.batstate-u.edu.ph>
2026-03-12 13:06:58 +0800

    add patients and doctors migration files with necessary fields

--- priv/repo/migrations/20260312045751_patients.exs
+++ priv/repo/migrations/20260312045751_patients.exs
@@ -2,5 +2,12 @@ defmodule Medigrant.Repo.Migrations.Patients do
   use Ecto.Migration
 
   def change do
+    create table(:patients) do
+      add :email, :string, null: false
+      add :name, :string, null: false
+      add :password_hash, :string, null: false
+
+      timestamps()
+    end
   end
 end
--- priv/repo/migrations/20260312045817_doctors.exs
+++ priv/repo/migrations/20260312045817_doctors.exs
@@ -2,6 +2,14 @@ defmodule Medigrant.Repo.Migrations.Doctors do
   use Ecto.Migration
 
   def change do
+    create table(:doctors) do
+      add :name, :string, null: false
+      add :specialization, :string, null: false
+      add :license_number, :string, null: false
 
+      timestamps()
+    end
+
+    create unique_index(:doctors, [:license_number])
   end
 end
